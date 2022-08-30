d74523945c3f810c53f765382ba955206536d2f5
KarlaV <karlista.ta@gmail.com>
2022-08-30 17:11:44 +0100

    Added the file kthLargest.py to the repository

---
+++ kthLargest.py
@@ -0,0 +1,20 @@
+import heapq
+class KthLargest:
+    def __init__(self, k, nums):
+
+        self.min_heap, self.k = nums, k
+        heapq.heapify(self.min_heap)
+        
+        while self.k < len(self.min_heap):
+            heapq.heappop(self.min_heap)
+        print(self.min_heap)
+
+    def add(self, val):
+        heapq.heappush(self.min_heap, val)
+        if self.k < len(self.min_heap):
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
+    
+    
+a = KthLargest(3, [4, 5, 8, 2]);
+print(a.add(3))
